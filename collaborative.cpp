54f9c72da002ff20b3871f9b73af4b826e8628db
Bibek Ghimire <bghimi4@lsu.edu>
2014-04-23 09:00:55 -0500

    adding average that condisers simi and all

--- collaborative.cpp
+++ collaborative.cpp
@@ -12,111 +12,272 @@
 #include <string>
 #include <math.h>
 #include <cmath>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 typedef vector<int> Row;
 typedef vector<Row> table;
-table my_table(4,Row(4)) ;
 
-double similarity(const vector<int> a_table,const vector<int> i_table,int size);
-double cal_avg(const vector<int> compute_this_table,const vector<int> compare_this_table);
-double paper_predict(const table,const vector<int>,int size,int location);
-double class_predict();
+
+double cal_simi_avg(const vector<int> compute_this_table,const vector<int> compare_this_table);
+double cal_avg(const vector<int> compute_this_table);
+
+void paper_predict_all(const table my_table, int active_user,
+                          int active_users_item_to_predict_vote );
+void paper_predict_simi(const table my_table, int active_user,
+                         int active_users_item_to_predict_vote );
+void class_predict_all(const table my_table, int active_user,
+                         int active_users_item_to_predict_vote);
+void class_predict_simi(const table my_table, int active_user,
+                          int active_users_item_to_predict_vote );
+void top_rating_item(int active_user, int compare_with_this_user);
+
+double simi_avg_simi(const vector<int> active_user,const vector<int> compare_to_user);
+//double simi_avg_all(const vector<int> active_user,const vector<int> compare_to_user);
+
+vector<int> parsing(string);
+void print(const table);
+int count_votes(vector<int>);
+vector<string> lines;
 
 int main(int argc, const char * argv[])
 {
     string line;
+    table my_table;
     ifstream myfile ("/Users/lsu/Documents/workspace/collaborative/collaborative/data.txt");
-    int i= 0,j=0,k=0;
-    int location_of_vote_to_figure_out = 0;
-    
+    int active_user = 3;
+    int item_number = 0;
+    cout<<"Begin .. "<<endl;
     if (myfile.is_open()){
         while ( getline (myfile,line) )
         {
-            if(line[j] != '#'){
-                while(line[j] != '\0' ){
-                    if(line[j] != ' '){
-                        my_table[i][k] = line[j] - '0';
-                        k++;
-                    }j++;
-                }j=0;k=0;i++;
+            lines.push_back(line);
+            if(parsing(line).size() > 0)
+            {
+                my_table.push_back(parsing(line));
+        
             }
         }
         myfile.close();
     }else{
         cout << "Unable to open file";
     }
-    for(int a =0;a <4;a++){
-        for (int b =0; b <4;b++) {
-            cout<<" "<<my_table[a][b]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<" Actually -3 means user have not voted for this item "<<endl;
-    cout<<"Begin the prediction..."<<endl;
-    cout<<paper_predict(my_table,my_table[3],(int)my_table[3].size(),location_of_vote_to_figure_out);
-    cout<<endl;
+    print(my_table);
+    cout<<"Prosessing .. paper prediction_All .. "<<endl;
+    paper_predict_all(my_table, active_user, item_number);
+    cout<<endl<<endl;
+    cout<<"Prosessing .. paper prediction_Simi .."<<endl;
+    paper_predict_simi(my_table, active_user, item_number);
+    cout<<endl<<endl;
+    cout<<"Prosessing .. class prediction_ALL .."<<endl;
+    class_predict_all(my_table, active_user, item_number);
+    cout<<endl<<endl;
+    cout<<"Prosessing .. class prediction_Simi .."<<endl;
+    class_predict_simi(my_table, active_user, item_number);
+    cout<<endl<<endl;
+    
     return 0;
 }
-
-double paper_predict(const table my_table,const vector<int> predict_this_user,int size,int location_of_vote_to_figure_out){
+vector<int> parsing(string each_line){
     
-    double my_avg = 0.0;
+    string dummy;
+    dummy.clear();
+    Row my_row;
+    
+    for(int i = 0; i < each_line.length(); i++){
+        
+        if(each_line[i] != ' '){
+            
+            dummy += each_line[i];
+            if(dummy[0] != 'i' && dummy[0] != 'u'){
+            
+                
+                if (dummy[0] == '-') {
+                    my_row.push_back(-1);
+                }
+                else{
+                    my_row.push_back(atoi(dummy.c_str()));
+                }
+                
+            }
+            
+               
+        }else{
+            dummy.clear();
+        }
+    }
+    return my_row;
+    
+}
+
+void print(table my_table){
+    //cout<<" the size of my_table = "<<my_table.size()<<endl;
+   /*
+    for (int i = 0 ; i < my_table.size(); i++) {
+        for(int j = 0; j < my_table[i].size(); j++){
+            if(my_table[i][j] > 0){
+                cout<<my_table[i][j]<<"  ";
+            }
+            else{
+                cout<<"-"<<"  ";
+            }
+        }
+        cout<<endl;
+    }*/
+    for (int k = 0; k < lines.size(); k++) {
+        cout<<lines[k]<<endl;
+    }
+}
+
+void paper_predict_all(const table my_table, int active_user,
+                     int active_users_item_to_predict_vote ){
+    
+    double avg_of_each_i = 0.0;
+    double k = 0.0;
+    double simi = 0.0;
+    double numerator = 0.0;
+    for (int j = 0; j < my_table.size(); j++) {
+        
+        if((my_table[j][active_users_item_to_predict_vote] > 0) && active_user != j ){
+            
+            simi = simi_avg_simi(my_table[active_user], my_table[j]);
+            k = k + abs(simi);
+            avg_of_each_i = cal_avg(my_table[j]);
+            cout<<" with user "<<j <<setw(10)<<"sim= " <<simi<<endl;
+            numerator = numerator + simi*(my_table[j][active_users_item_to_predict_vote]-avg_of_each_i);
+        
+        }
+        
+    }
+    cout<<"Predicted value for user "<< active_user<< " and item "<<active_users_item_to_predict_vote<<" = "
+    <<( cal_avg(my_table[active_user])+ (numerator/k));
+    
+}
+
+void paper_predict_simi(const table my_table, int active_user,
+                         int active_users_item_to_predict_vote ){
+    
+    double avg_of_each_i = 0.0;
+    double k = 0.0;
+    double simi = 0.0;
+    double numerator = 0.0;
+    
+    for (int j = 0; j < my_table.size(); j++) {
+        
+        if(my_table[j][active_users_item_to_predict_vote]>0 && active_user != j ){
+            
+            simi = simi_avg_simi(my_table[active_user], my_table[j]);
+            k = k + abs(simi);
+            avg_of_each_i = cal_simi_avg(my_table[j],my_table[active_user]);
+            cout<<" with user "<<j <<setw(10)<<"sim= " <<simi<<endl;
+            numerator = numerator + simi*(my_table[j][active_users_item_to_predict_vote]-avg_of_each_i);
+        }
+    }
+    cout<<"Predicted value for user "<< active_user<< " and item "<<active_users_item_to_predict_vote<<" = "
+    <<( cal_avg(my_table[active_user])+ (numerator/k));
+}
+
+void class_predict_simi(const table my_table, int active_user,
+                          int active_users_item_to_predict_vote ){
+
     double avg_of_each_i = 0.0;
     int count = 0;
     double k = 0.0;
     double simi = 0.0;
     double numerator = 0.0;
-    for (int i = 0; i < size; i++) {
-        if(predict_this_user[i] > 0){
-            my_avg = my_avg + predict_this_user[i];
-            count++;
-        }
-    }
-    my_avg = my_avg/count;
-    cout<<"my_avg = "<<my_avg<<endl;
-    
+    count = count_votes(my_table[active_user]);
+
     for (int j = 0; j < my_table.size(); j++) {
-        if(my_table[j][location_of_vote_to_figure_out]>0 && predict_this_user != my_table[j] ){
-            simi = similarity(predict_this_user, my_table[j], size);
+        if(my_table[j][active_users_item_to_predict_vote ]>0 && active_user != j ){
+            simi = simi_avg_simi(my_table[active_user], my_table[j]);
             k = k + abs(simi);
-            avg_of_each_i = cal_avg(my_table[j],predict_this_user);
-            cout<<"sim= " <<simi <<",average= "<<avg_of_each_i<<",";
-            numerator = numerator + simi*(my_table[j][location_of_vote_to_figure_out]-avg_of_each_i);
-            cout<< "numerator= "<<numerator<<",";
-            cout<<"j= "<<my_table[j][location_of_vote_to_figure_out]<<endl;
+            avg_of_each_i = cal_simi_avg(my_table[j],my_table[active_user]);
+            cout<<" with user "<<j <<setw(10)<<"sim= " <<simi<<endl;
+            numerator = numerator + simi*(my_table[j][active_users_item_to_predict_vote]-avg_of_each_i);
+            
         }
-        
     }
-    cout<<"numerator = "<<numerator<<endl;
-    return (my_avg + (numerator/k));
+    cout<<"Predicted value for user "<< active_user<< " and item "<<active_users_item_to_predict_vote<<" = "
+    <<( ((cal_avg(my_table[active_user])*count) + ((numerator/k)*(count+1)))/count);
 }
 
-double similarity(const vector<int> a_table,const vector<int> i_table,int size){
+void class_predict_all(const table my_table, int active_user,
+                         int active_users_item_to_predict_vote){
     
-    int size_of_a = size ;
-    double avg_of_a = cal_avg(a_table,i_table);
-    double avg_of_i = cal_avg(i_table,a_table);
+    double avg_of_each_i = 0.0;
+    int count = 0;
+    double k = 0.0;
+    double simi = 0.0;
     double numerator = 0.0;
+    count = count_votes(my_table[active_user]);
+
+    for (int j = 0; j < my_table.size(); j++) {
+        if(my_table[j][active_users_item_to_predict_vote]>0 && active_user != j ){
+            simi = simi_avg_simi(my_table[active_user], my_table[j]);
+            k = k + abs(simi);
+            avg_of_each_i = cal_avg(my_table[j]);
+            cout<<" with user "<<j <<setw(10)<<"sim= " <<simi<<endl;
+            numerator = numerator + simi*(my_table[j][active_users_item_to_predict_vote]-avg_of_each_i);
+            
+        }
+    }
+    cout<<"Predicted value for user "<< active_user<< " and item "<<active_users_item_to_predict_vote<<" = "
+    << (((cal_avg(my_table[active_user])*count) + ((numerator/k)*(count+1)))/count);
+}
+
+double simi_avg_simi(const vector<int> active_user,const vector<int> compare_to_user){
+
+    double avg_of_active_user  = cal_simi_avg(active_user,compare_to_user);
+    double avg_of_compare_user = cal_simi_avg(compare_to_user,active_user);
+    double numerator    = 0.0;
     double denominator1 = 0.0;
     double denominator2 = 0.0;
-    for (int i =0; i<size_of_a;i++) {
-        if(a_table[i] > 0 && i_table[i]>0){
-            numerator = numerator+((a_table[i]-avg_of_a)*(i_table[i]-avg_of_i));
-            denominator1  = denominator1 + ((a_table[i]-avg_of_a)*(a_table[i]-avg_of_a));
-            denominator2  = denominator2 + ((i_table[i]-avg_of_i)*(i_table[i]-avg_of_i));
+    
+    for (int i =0; i<active_user.size();i++) {
+        if(active_user[i] > 0 && compare_to_user[i]>0){
+            numerator = numerator+((active_user[i]-avg_of_active_user)*(compare_to_user[i]-avg_of_compare_user));
+            denominator1  = denominator1 + ((active_user[i]-avg_of_active_user)*(active_user[i]-avg_of_active_user));
+            denominator2  = denominator2 + ((compare_to_user[i]-avg_of_compare_user)*(compare_to_user[i]-avg_of_compare_user));
+        }
+    }
+
+    return (numerator/sqrt(denominator1*denominator2));
+}
+/*
+double simi_avg_all(const vector<int> active_user,const vector<int> compare_to_user){
+    
+    double avg_of_active_user  = cal_simi_avg(active_user,compare_to_user);
+    double avg_of_compare_user = cal_simi_avg(compare_to_user,active_user);
+    double numerator    = 0.0;
+    double denominator1 = 0.0;
+    double denominator2 = 0.0;
+    
+    for (int i =0; i<active_user.size();i++) {
+        if(active_user[i] > 0 && compare_to_user[i]>0){
+            numerator = numerator+((active_user[i]-avg_of_active_user)*(compare_to_user[i]-avg_of_compare_user));
+            denominator1  = denominator1 + ((active_user[i]-avg_of_active_user)*(active_user[i]-avg_of_active_user));
+            denominator2  = denominator2 + ((compare_to_user[i]-avg_of_compare_user)*(compare_to_user[i]-avg_of_compare_user));
         }
     }
     return (numerator/sqrt(denominator1*denominator2));
 }
-
-double cal_avg(const vector<int> compute_this_table,const vector<int> compare_this_table){
+ */
+void top_rating_item(int active_user, int compare_with_this_user){
     
-    int size =(int) compute_this_table.size();
+    
+    cout<<"Printing top three item for user "<<active_user<< "is = ";
+    
+    
+    
+}
+
+double cal_simi_avg(const vector<int> compute_this_table,const vector<int> compare_this_table){
+    
     double sum = 0.0;
     int count=0;
-    for (int i=0; i<size; i++) {
+    for (int i=0; i<compute_this_table.size(); i++) {
         if(compute_this_table[i] > 0 && compare_this_table[i] > 0)
         {
             sum = compute_this_table[i]+sum;
@@ -126,4 +287,29 @@ double cal_avg(const vector<int> compute_this_table,const vector<int> compare_th
     return sum/count;
 }
 
+double cal_avg(const vector<int> compute_this_table){
+    
+    double sum = 0.0;
+    int count = 0;
+    
+    for (int i =0; i < compute_this_table.size(); i++) {
+        if (compute_this_table[i] > 0) {
+            sum = compute_this_table[i] + sum;
+            count++;
+        }
+    }
+    return sum/count;
+}
+
+int count_votes(vector<int> count_this_vector){
+    
+    int count= 0;
+    
+    for (int i = 0; i < count_this_vector.size(); i++) {
+        if(count_this_vector[i] > 0){
+            count++;
+        }
+    }
+    return count;
+}
 
