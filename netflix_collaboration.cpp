d28ff92bd3d74788ea0158cb9722ab7bb3f93933
Bibek Ghimire <bghimi4@lsu.edu>
2014-05-08 22:26:00 -0500

    this code is slow because it has to search for the item

--- netflix_collaboration.cpp
+++ netflix_collaboration.cpp
@@ -23,24 +23,27 @@ struct ItemRating{
     int user;
     int item;
     int rating;
-    double Avg;
+    int totalsum;
+
 };
 
 
 typedef vector<ItemRating> Row;
 typedef vector<Row> table;
+vector<vector<double>> HoldSimiAvg;
 vector<int> parsing(string);
 void print(const table);
 double PredictionEq1(const table MyTable, int ActiveUser,
                        int PredictVoteOfItem);
-double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser);
-void CalAvg(table& );
+double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser, double, double);
 double CalSimiAvg(const vector<ItemRating>,const vector<ItemRating>);
 int HasItemAt(const table MyTable, int User,
                    int Item);
 table Filling_Table(table,string);
 double root_mean_square(table BaseTable, table TestTable);
-
+void CalTotalItemRatingOfAll(table& BaseTable);
+void CalTotalSimiRatingOfAll(table BaseTable,vector<vector<double>>&);
+int getSizeOfItemsUserRated(vector<ItemRating> ItemsOfUser);
 
 
 int main(int argc, const char * argv[])
@@ -54,12 +57,12 @@ int main(int argc, const char * argv[])
     
     BaseTable = Filling_Table(BaseTable,"/Users/bghimire/workspace/neflix_prediction_algorithm/neflix_prediction_algorithm/u1base.txt");
     TestTable = Filling_Table(TestTable,"/Users/bghimire/workspace/neflix_prediction_algorithm/neflix_prediction_algorithm/u1test.txt");
-    
+    CalTotalItemRatingOfAll(BaseTable );
+    CalTotalSimiRatingOfAll(BaseTable, HoldSimiAvg);
     cout<<"Begin .. "<<endl;
-    //print(TestTable);
+    //print(BaseTable);
     
     start = std::clock();
-    CalAvg(BaseTable);
     MSSE  = root_mean_square(BaseTable, TestTable);
     cout << " Total Mean sum of square Error = " << MSSE << endl;
     cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC ) << "  sec" << std::endl;
@@ -155,37 +158,91 @@ void print(table my_table){
         cout<<" next user "<<endl;
     }
 }
+
+double root_mean_square(table BaseTable, table TestTable){
+    
+    double actual_value    = 0.0;
+    double predicted_value = 0.0;
+    double MSSE            = 0.0;
+    int count              = 0;
+    for (int i = 0; i < TestTable.size(); i++){
+        for (int j = 0; j<TestTable[i].size(); j++) {
+            predicted_value = PredictionEq1(BaseTable, TestTable[i][j].user, TestTable[i][j].item);
+            cout<<" predicted vote for user "<< TestTable[i][j].user << " and item "<<TestTable[i][j].item<<"  = "<<predicted_value<<endl;
+            actual_value    = TestTable[i][j].rating;
+            MSSE            = (pow((actual_value - predicted_value), 2));
+            count++;
+        }
+    }
+    
+    return (MSSE/count);
+}
+
 /* This function predicts the rating of the user using 
  * eq1 of the paper
  */
-double PredictionEq1(const table MyTable, int ActiveUser,
+double PredictionEq1(const table BaseTable, int ActiveUser,
                        int PredictVoteOfItem){
     
     double k = 0.0;
     double simi = 0.0;
     double numerator = 0.0;
-    double avg_of_each_comparing_user = 0.0;
-    int similar_item;
-    double Avg_of_i = 0.0;
-    for (int i = 0; i < MyTable.size(); i++) {
+    double AvgOfEachComparingUser = 0.0;
+    int similar_item = -1;
+    int TrackSimi = 0;
+    double AvgOfActiveUser = 0.0;
+    //cout<<"BaseTable Size  = "<<BaseTable.size()<<endl;
+    AvgOfActiveUser = (double)BaseTable[ActiveUser-1][0].totalsum/(double)BaseTable[ActiveUser-1].size();
+    //cout<<"AvgOfActiveUser = "<<AvgOfActiveUser<<endl;
+    for (int i = 0; i < BaseTable.size(); i++) {
         
-        similar_item = HasItemAt(MyTable,i,PredictVoteOfItem);
-        if(i != (MyTable[ActiveUser][0].user-1) && (similar_item >= 0)){
-            
-            simi = SimiCorrelation(MyTable[ActiveUser], MyTable[i]);
+        similar_item = HasItemAt(BaseTable,i,PredictVoteOfItem);
+        //cout<<"similar_item = "<<similar_item<<"   "<<endl;
+        if(i != (ActiveUser-1) && (similar_item < 0)) { TrackSimi++;}
+        if(i != (ActiveUser-1) && (similar_item >= 0)){ // ActiveUser-1 because i start from 0
+            //cout<<"("<<(ActiveUser-1)<<","<<TrackSimi<<")"<<endl;
+            simi = HoldSimiAvg[ActiveUser-1][TrackSimi];//= SimiCorrelation(BaseTable[ActiveUser-1], BaseTable[i]);
             k = k + abs(simi);
-            avg_of_each_comparing_user = CalSimiAvg(MyTable[i],MyTable[ActiveUser]);
-            numerator = numerator + simi*(MyTable[i][similar_item].rating -
-                                   avg_of_each_comparing_user);
-
+            //cout<<"For user                   =  "<<BaseTable[i][similar_item].user<<endl;
+            AvgOfEachComparingUser = (double)(BaseTable[i][0].totalsum - BaseTable[i][similar_item].rating)/(double)(getSizeOfItemsUserRated(BaseTable[i])-1);
+            //cout<<"First term and second term =  "<<BaseTable[i][similar_item].rating<<" - "<<AvgOfEachComparingUser<<endl;
+            //cout<<"simi                       =  "<<simi<<endl;
+            numerator = numerator + simi*(BaseTable[i][similar_item].rating -
+                                          AvgOfEachComparingUser);
+            
+            TrackSimi++;
         }
+        
+        
     }
     
-    return (Avg_of_i+ numerator/k);
+    return (AvgOfActiveUser+ numerator/k);
     
 }
+
+void CalTotalSimiRatingOfAll(table BaseTable, vector<vector<double>>& HoldSimiAvg){
+    
+    vector<double> simi;
+    double value = 0.0;
+    for (int i = 0; i < BaseTable.size(); i++) {
+        for (int j  = 0; j < BaseTable.size(); j++) {
+            if(i != j){
+                double AvgOfActiveUser  = CalSimiAvg(BaseTable[i], BaseTable[j]);
+                double AvgOfCompareUser = CalSimiAvg(BaseTable[j], BaseTable[i]);
+                value  = SimiCorrelation(BaseTable[i], BaseTable[j],AvgOfActiveUser,AvgOfCompareUser);
+                //cout<<"simi between activer user "<<i <<" and compare to user "<< j << " = " << value <<endl;
+                simi.push_back(value);
+                
+            }
+        }
+        HoldSimiAvg.push_back(simi);
+        simi.clear();
+    }
+
+}
+
 /*
- returns if the index where the specificific item resides
+ returns if the index-1 where the specificific item resides
  else return -1 to indicate that there is no such item 
  in the array
  */
@@ -211,11 +268,8 @@ int HasItemAt(const table MyTable,int User,
  * This function calculates the correlation between the 
  * active user and the other user that is compared with
   */
-double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser){
-    
-    double AvgOfActiveUser  = CalSimiAvg(ActiveUser, CompareToUser);
-    double AvgOfCompareUser = CalSimiAvg(CompareToUser, ActiveUser);
-   
+double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser, double AvgOfActiveUser, double AvgOfCompareUser){
+
     double numerator    = 0.0;
     double denominator1 = 0.0;
     double denominator2 = 0.0;
@@ -245,27 +299,11 @@ double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRati
     else return (numerator/sqrt(denominator1*denominator2));
 }
 
-/* 
- * calculates the average rating of the items rated by the
- * user
- */
-
-
-void CalAvg(table& BaseTable){
-    
-    double sum = 0.0;
-    for (int i = 0; i < BaseTable.size(); i++) {
-        for (int j = 0; j < BaseTable[i].size(); j++) {
-            
-            
-        }
-    }
-    
-}
-
-/* calculates the average rating of the similar items 
+/* calculates the average rating of the similar items
  * present between the active user the compared user
  */
+
+
 
 double CalSimiAvg(const vector<ItemRating> active_user, const vector<ItemRating> compare_user){
     
@@ -296,21 +334,32 @@ double CalSimiAvg(const vector<ItemRating> active_user, const vector<ItemRating>
     }
 }
 
-double root_mean_square(table BaseTable, table TestTable){
+/* 
+ * calculates the total sum of all the rating rated by the
+ * user
+ */
+
+
+void CalTotalItemRatingOfAll(table& BaseTable){
     
-    double actual_value    = 0.0;
-    double predicted_value = 0.0;
-    double MSSE            = 0.0;
-    int count              = 0;
-    for (int i = 0; i < TestTable.size(); i++){
-        for (int j = 0; j<TestTable[i].size(); j++) {
-            predicted_value = PredictionEq1(BaseTable, TestTable[i][j].user, TestTable[i][j].item);
-            cout<<" predicted vote for user "<< TestTable[i][j].user << " and item "<<TestTable[i][j].item<<"  = "<<predicted_value<<endl;
-            actual_value    = TestTable[i][j].rating;
-            MSSE            = (pow((actual_value - predicted_value), 2));
-            count++;
+    double sum = 0.0;
+    for (int i = 0; i < BaseTable.size(); i++) {
+        for (int j = 0; j < BaseTable[i].size(); j++) {
+            //cout<<"Inside CaltotalItemRatingOfAll of user "<<i<<" and item "<<j<<" rating  = "<<BaseTable[i][j].rating<<endl;
+            sum = sum + BaseTable[i][j].rating;
         }
+        BaseTable[i][0].totalsum = sum;
+        //cout << "Total sum = "<<sum<<endl;
+        sum = 0;
     }
+    //return sum;
+}
+
+
+
+
+
+int getSizeOfItemsUserRated(vector<ItemRating> ItemsOfUser){
     
-    return (MSSE/count);
+    return (int)ItemsOfUser.size();
 }
