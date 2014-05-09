f870c2ea8df9514dd78b5af5d732f36db15d3ce0
bghimire <bghimi4@lsu.edu>
2014-05-09 14:54:23 -0500

    uses eq1 and eq2 to predict

--- netflix_collaboration.cpp
+++ netflix_collaboration.cpp
@@ -37,8 +37,12 @@ vector<vector<double>> HoldSimiAvg;
 vector<int> parsing(string);
 void print(const table);
 double PredictionEq1(const table MyTable, int ActiveUser,
-                       int PredictVoteOfItem);
-double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser, double, double);
+                       int PredictVoteOfItem); // Paper Eq
+double PredictionEq2(const table BaseTable, int ActiveUser,
+                     int PredictVoteOfItem); // In class Modified Eq
+double SimiCorrelation(const vector<ItemRating> ActiveUser,
+                       const vector<ItemRating> CompareToUser, double, double);
+
 double CalSimiAvg(const vector<ItemRating>,const vector<ItemRating>);
 int HasItemAt(const table MyTable, int User,
                    int Item);
@@ -60,20 +64,24 @@ int main(int argc, const char * argv[])
     clock_t start;
     double MSSE = 0.0;
     
-    BaseTable = Filling_Table(BaseTable,"/Users/lsu/Documents/workspace/NetflixPredictionAlgorithm/NetflixPredictionAlgorithm/u1base.txt","base");
-    TestTable = Filling_Table(TestTable,"/Users/lsu/Documents/workspace/NetflixPredictionAlgorithm/NetflixPredictionAlgorithm/u1test.txt","test");
+    BaseTable = Filling_Table(BaseTable,
+                              "/Users/bghimire/workspace/neflix_prediction_algorithm/neflix_prediction_algorithm/u1base.txt","base");
+    TestTable = Filling_Table(TestTable,
+                              "/Users/bghimire/workspace/neflix_prediction_algorithm/neflix_prediction_algorithm/u1test.txt","test");
     cout<<"STARING"<<endl;
+    start = std::clock();
     CalTotalItemRatingOfAll(BaseTable );
     CalTotalSimiRatingOfAll(BaseTable, HoldSimiAvg);
     cout<<"Begin .. "<<endl;
     //print(BaseTable);
     //cout<<"------------------"<<endl;
+    
     //modifiedprint(BaseTable);
     
-    start = std::clock();
+    
     MSSE  = root_mean_square(BaseTable, TestTable);
     cout << " Total Mean sum of square Error = " << MSSE << endl;
-    cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC ) << "  sec" << std::endl;
+    cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC ) << "  sec or " <<((std::clock() - start) / (double)(CLOCKS_PER_SEC ))/60<<" min "<< std::endl;
     
     return 0;
 }
@@ -167,11 +175,15 @@ table Filling_Table(table BaseTable,string filepath,string type){
     return BaseTable;
 }
 
+/*
+ * Used for debugging, prints the map's item and key
+ *
+ */
 void modifiedprint(table my_table){
-    
+    int TotalItem = 1682;
     string key = "I";
     for (int i = 0 ; i < my_table.size(); i++) {
-        for(int j = 0; j < my_table[i].size(); j++){
+        for(int j = 0; j < TotalItem; j++){
             key.append(to_string(i+1)).append("-").append(to_string(j+1));
             cout<<key<<" = ";
             cout<<Map[key]<<endl;
@@ -193,11 +205,19 @@ void print(table my_table){
     
     for (int i = 0 ; i < my_table.size(); i++) {
         for(int j = 0; j < my_table[i].size(); j++){
-            cout<<"user = "<<my_table[i][j].user<<", item = " <<my_table[i][j].item<< " , raing = " << my_table[i][j].rating<< "  "<< ", key = "<<my_table[i][j].key <<",value = "<<Map[my_table[i][j].key]<<endl;
+            cout<<"user = "<<my_table[i][j].user<<", item = " <<my_table[i][j].item<<
+            " , raing = " << my_table[i][j].rating<< "  "<< ", key = "<<my_table[i][j].key <<
+            ",value = "<<Map[my_table[i][j].key]<<endl;
         }
         cout<<" next user "<<endl;
     }
 }
+
+/*
+ * Calculates the Mean Sum of Square of Error between the predicted value
+ * and the test value
+ *
+ */
 
 double root_mean_square(table BaseTable, table TestTable){
     
@@ -209,9 +229,10 @@ double root_mean_square(table BaseTable, table TestTable){
     for (int i = 0; i < TestTable.size(); i++){
         for (int j = 0; j<TestTable[i].size(); j++) {
             //start = std::clock();
-            predicted_value = PredictionEq1(BaseTable, TestTable[i][j].user, TestTable[i][j].item);
+            predicted_value = PredictionEq2(BaseTable, TestTable[i][j].user, TestTable[i][j].item);
             //cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC ) << "  sec" << std::endl;
-            cout<<" predicted vote for user "<< TestTable[i][j].user << " and item "<<TestTable[i][j].item<<"  = "<<predicted_value<<endl;
+            //cout<<" predicted vote for user "<< TestTable[i][j].user <<
+            //" and item "<<TestTable[i][j].item<<"  = "<<predicted_value<<endl;
             actual_value    = TestTable[i][j].rating;
             MSSE            = MSSE + (pow((actual_value - predicted_value), 2));
             count++;
@@ -235,29 +256,19 @@ double PredictionEq1(const table BaseTable, int ActiveUser,
     int TrackSimi = 0;
     double AvgOfActiveUser = 0.0;
     string key = "I";
-    //clock_t start;
-    //cout<<"BaseTable Size  = "<<BaseTable.size()<<endl;
     AvgOfActiveUser = (double)BaseTable[ActiveUser-1][0].totalsum/(double)BaseTable[ActiveUser-1].size();
-    //cout<<"AvgOfActiveUser = "<<AvgOfActiveUser<<endl;
     for (int i = 0; i < BaseTable.size(); i++) {
-        //start = std::clock();
-        //similar_item = ModifiedHasItemAt(BaseTable,i,PredictVoteOfItem);
         key.append(to_string(i+1)).append("-").append(to_string(PredictVoteOfItem));
         similar_item  = Map[key];
-        //cout<<"calculate simi of "<<key<< " = " <<similar_item<<endl;
         key.clear();
         key = "I";
-        //cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC ) << "  sec" << std::endl;
-        //cout<<"similar_item = "<<similar_item<<"   "<<endl;
         if(i != (ActiveUser-1) && (similar_item == 0)) { TrackSimi++;}
         if(i != (ActiveUser-1) && (similar_item > 0)){ // ActiveUser-1 because i start from 0
             //cout<<"("<<(ActiveUser-1)<<","<<TrackSimi<<")"<<endl;
             simi = HoldSimiAvg[ActiveUser-1][TrackSimi];//= SimiCorrelation(BaseTable[ActiveUser-1], BaseTable[i]);
             k = k + abs(simi);
-            //cout<<"For user                   =  "<<BaseTable[i][similar_item].user<<endl;
-            AvgOfEachComparingUser = (double)(BaseTable[i][0].totalsum - BaseTable[i][similar_item-1].rating)/(double)(getSizeOfItemsUserRated(BaseTable[i])-1);
-            //cout<<"First term and second term =  "<<BaseTable[i][similar_item].rating<<" - "<<AvgOfEachComparingUser<<endl;
-            //cout<<"simi                       =  "<<simi<<endl;
+            AvgOfEachComparingUser = (double)(BaseTable[i][0].totalsum - BaseTable[i]
+                                              [similar_item-1].rating)/(double)(getSizeOfItemsUserRated(BaseTable[i])-1);
             numerator = numerator + simi*(BaseTable[i][similar_item-1].rating -
                                           AvgOfEachComparingUser);
             
@@ -272,6 +283,52 @@ double PredictionEq1(const table BaseTable, int ActiveUser,
     
 }
 
+
+/* This function predicts the rating of the user using
+ * eq2 modified eq of class
+ */
+double PredictionEq2(const table BaseTable, int ActiveUser,
+                     int PredictVoteOfItem){
+    
+    double k = 0.0;
+    double simi = 0.0;
+    double numerator = 0.0;
+    double AvgOfEachComparingUser = 0.0;
+    int similar_item = -1;
+    int TrackSimi = 0;
+    //double AvgOfActiveUser = 0.0;
+    string key = "I";
+    //AvgOfActiveUser = (double)BaseTable[ActiveUser-1][0].totalsum/(double)BaseTable[ActiveUser-1].size();
+    for (int i = 0; i < BaseTable.size(); i++) {
+        key.append(to_string(i+1)).append("-").append(to_string(PredictVoteOfItem));
+        similar_item  = Map[key];
+        key.clear();
+        key = "I";
+        if(i != (ActiveUser-1) && (similar_item == 0)) { TrackSimi++;}
+        if(i != (ActiveUser-1) && (similar_item > 0)){ // ActiveUser-1 because i start from 0
+            simi = HoldSimiAvg[ActiveUser-1][TrackSimi];//= SimiCorrelation(BaseTable[ActiveUser-1], BaseTable[i]);
+            k = k + abs(simi);
+            AvgOfEachComparingUser = (double)(BaseTable[i][0].totalsum)/(double)(getSizeOfItemsUserRated(BaseTable[i]));
+            numerator = numerator + simi*(BaseTable[i][similar_item-1].rating -
+                                          AvgOfEachComparingUser);
+            
+            TrackSimi++;
+        }
+        
+        
+    }
+    if(k==0) return 0;
+    
+    return ((double)BaseTable[ActiveUser-1][0].totalsum+ ((numerator/k)*(double)BaseTable[ActiveUser-1].size()))/((double)BaseTable[ActiveUser-1].size()-1);
+    
+}
+
+/*
+ * Calculates the similarity between users and push it to the
+ * 2D vector of HoldSimiAvg. HoldSimiAvg[0] hold the simi between
+ * user 1 and n users, HoldSimiAvg[1] hols the simi between user 2 and n
+ * users
+ */
 void CalTotalSimiRatingOfAll(table BaseTable, vector<vector<double>>& HoldSimiAvg){
     
     vector<double> simi;
@@ -292,7 +349,9 @@ void CalTotalSimiRatingOfAll(table BaseTable, vector<vector<double>>& HoldSimiAv
     }
 
 }
-
+/*
+ * Uses Binary Search to look up item
+ */
 int ModifiedHasItemAt(const table MyTable, int User,
               int Item)
 {
@@ -341,7 +400,8 @@ int HasItemAt(const table MyTable,int User,
  * This function calculates the correlation between the 
  * active user and the other user that is compared with
   */
-double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating> CompareToUser, double AvgOfActiveUser, double AvgOfCompareUser){
+double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRating>
+                       CompareToUser, double AvgOfActiveUser, double AvgOfCompareUser){
 
     double numerator    = 0.0;
     double denominator1 = 0.0;
@@ -353,9 +413,12 @@ double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRati
     while( (i < ActiveUser.size()) && (j < CompareToUser.size())){
         if(ActiveUser[i].item == CompareToUser[j].item ){
             
-            numerator = numerator+((ActiveUser[i].rating-AvgOfActiveUser)*(CompareToUser[j].rating-AvgOfCompareUser));
-            denominator1  = denominator1 + ((ActiveUser[i].rating-AvgOfActiveUser)*(ActiveUser[i].rating-AvgOfActiveUser));
-            denominator2  = denominator2 + ((CompareToUser[j].rating-AvgOfCompareUser)*(CompareToUser[j].rating-AvgOfCompareUser));
+            numerator = numerator+((ActiveUser[i].rating-AvgOfActiveUser)*
+                                   (CompareToUser[j].rating-AvgOfCompareUser));
+            denominator1  = denominator1 + ((ActiveUser[i].rating-AvgOfActiveUser)*
+                                            (ActiveUser[i].rating-AvgOfActiveUser));
+            denominator2  = denominator2 + ((CompareToUser[j].rating-AvgOfCompareUser)*
+                                            (CompareToUser[j].rating-AvgOfCompareUser));
             }
         tmp = j;
         if(ActiveUser[i].item >= CompareToUser[tmp].item){
@@ -375,8 +438,6 @@ double SimiCorrelation(const vector<ItemRating> ActiveUser,const vector<ItemRati
 /* calculates the average rating of the similar items
  * present between the active user the compared user
  */
-
-
 
 double CalSimiAvg(const vector<ItemRating> active_user, const vector<ItemRating> compare_user){
     
@@ -418,7 +479,8 @@ void CalTotalItemRatingOfAll(table& BaseTable){
     double sum = 0.0;
     for (int i = 0; i < BaseTable.size(); i++) {
         for (int j = 0; j < BaseTable[i].size(); j++) {
-            //cout<<"Inside CaltotalItemRatingOfAll of user "<<i<<" and item "<<j<<" rating  = "<<BaseTable[i][j].rating<<endl;
+            //cout<<"Inside CaltotalItemRatingOfAll of user "<<i<<"
+            //and item "<<j<<" rating  = "<<BaseTable[i][j].rating<<endl;
             sum = sum + BaseTable[i][j].rating;
         }
         BaseTable[i][0].totalsum = sum;
