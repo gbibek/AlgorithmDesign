b6ddcc75dd774a43d391853ea402139ea951875e
Bibek Ghimire <bibekghimire@bibeks-macbook-pro.local>
2016-02-03 16:40:25 -0600

    Initial Commit

---
+++ QuickSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickSort
+//
+//  Created by Bibek Ghimire on 2/3/16.
+//  Copyright © 2016 Bibek Ghimire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
